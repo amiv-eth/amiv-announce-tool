f6b853d99a03d2bca6868efc92395b563bf08019
Scheremo <scheremo@student.ethz.ch>
2015-11-15 13:08:22 +0100

    Overall updates

--- events.js
+++ events.js
@@ -4,35 +4,46 @@
 
 /* Create List Based on JSON */
 /* https://amiv-apidev.vsos.ethz.ch/events?where={%22show_announce%22:%20true} */
-$.getJSON( "https://amiv-apidev.vsos.ethz.ch/events", function( data ) {
-
-  var html = "<ul id='sortableList'>";
-
-  $.each( data._items, function( key, val ) {
-    html += "<li id='" + this.id + "'>Event ID " + this.id + "</li>";
-  });
-
-  html += "</ul>";
-
-  $( ".events" ).append(html);
 
 
-  /* Make List sortable */
-  $( "#sortableList" )
-      .sortable({ handle: ".handle" })
-      .selectable({ filter: "li", cancel: ".handle" })
-      .find( "li" )
-          .addClass( "ui-corner-all" )
-          .prepend( "<div class='handle'><span class='ui-icon ui-icon-carat-2-n-s'></span></div>"
-  );
+$(document).ready(function(){
+    $.getScript("https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css");
+    $.getJSON( "https://amiv-apidev.vsos.ethz.ch/events", function( data ) {
+	
+	var html = "<ul id='sortableList'>";
+	
+	html += "<style>.selected{}li{width:20%;}</style>";
+	
+	$.each( data._items, function( key, val ) {
+	    html +="<li id='" + this.id + "'>" + this.title_de + "</li>";
+	});
+	
+	html += "</ul>";
+	
+	$( ".events" ).append(html);
+	
+	$("#sortableList").sortable();
 
+	$("li").click(function(){
+	    if($(this).css("background-color")=="rgb(255, 0, 0)"){
+		$(this).css("background-color", "white");
+		$(this).removeClass(".selected");
+	    } else {
+		$(this).css("background-color","red");
+		$(this).addClass(".selected");
+	    }
+	});
 
-  /* Get selected IDs */
-  $('button').click(function() {
-      var ids = $('.ui-selected').map(function() {
-          return this.id;
-      });
-      alert('id: ' + ids.toArray().join(','));
-  });
+	/* Get selected IDs */
+	$('button').click(function() {
+	    var arr = [];
+	    var elements = document.getElementsByClassName(".selected");
+	    $.each(elements, function(index, value){
+		arr.push(this.id);
+	    });
 
-});
+	    console.log(arr);
+	    alert('id: ' + arr);
+	});
+    });
+})
