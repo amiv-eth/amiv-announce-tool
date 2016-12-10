bf139801717946a2dc459a2db3c0d5bbe00a53b5
Scheremo <scheremo@student.ethz.ch>
2016-12-10 09:42:13 +0100

    General overhaul of events.js and index.html to implement Bootstrap and conform to higher standards

--- events.js
+++ events.js
@@ -5,41 +5,52 @@
 /* Create List Based on JSON */
 /* https://amiv-apidev.vsos.ethz.ch/events?where={%22show_announce%22:%20true} */
 
+var URL = "http://192.168.1.100";
+var events = URL + "/events";
 
 $(document).ready(function(){
-    $.getJSON( "https://amiv-apidev.vsos.ethz.ch/events", function( data ) {
+    $.getJSON( events, function( data ) {
 
-	var posturl = "./request_announce.php";
 	var out = "";
 	
-	var html = "<ul id='sortableList'>";
+	var html = "<table class='table' id='mytable'><tbody class='sortableList'>";
 	
-	html += "<style>.selected{}.featured{}li{width:20%; background-color:white;}</style>";
-	
+	html += "<tr><th>ID</th><th>Title</th></tr>";
 	$.each( data._items, function( key, val ) {
 	    if(this.show_announce==true){
-		html +="<li id='" + this.id + "'>" + this.title_de + " </li>";
+		html +="<tr><td  id='" + this._id + "'><div>" + this._id + "</td><td>"+this.title_en +" </td></div></tr>";
 	    }
 	});
 	
-	html += "</ul>";
+	html += "</tbody></table>";
 	
-	$( ".events" ).append(html);
+	$( function(){
+	    $( "#events" ).append(html);
+	});
+	$(  function() {
+	    $(".sortableList").sortable();
+	});
+
 	
-	$("#sortableList").sortable();
-	
-	$("li").click(function(){
-	    if($(this).css("background-color")=="rgb(255, 0, 0)"){
+	$("div").click(function(){
+	    
+	    if($(this).hasClass("featured")){
+		$(this).removeClass("featured");
+	    } else{
+		$(this).addClass("featured").siblings().removeClass("featured");
+	    }
+	    
+	    /*if(!($(this).hasClass(".featured"))){
 		$(this).css("background-color", "rgb(0, 255, 0)");
 		$(this).addClass(".featured");
-	    } else if($(this).css("background-color")=="rgb(255, 255, 255)"){
-		$(this).css("background-color","rgb(255, 0, 0)");
-		$(this).addClass(".selected");
-	    } else if($(this).css("background-color")=="rgb(0, 255, 0)"){
+	    }  else if($(this).css("background-color")=="rgb(0, 255, 0)"){
 		$(this).css("background-color", "rgb(255, 255, 255)");
 		$(this).removeClass(".selected");
 		$(this).removeClass(".featured");
-	    }
+	    } else {
+		$(this).css("background-color","rgb(255, 0, 0)");
+		$(this).addClass(".selected");
+	    }*/
 	});
 	
 	/* Get selected IDs */
@@ -81,4 +92,4 @@ $(document).ready(function(){
 	    }
 	});
     });
-})
+}) 
