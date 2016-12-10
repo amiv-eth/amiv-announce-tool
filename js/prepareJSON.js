1323828d8e9b4144a3a4a712291503889c05cf89
Jan-Philipp Schulze <papierkorb@acrylschaf.de>
2016-12-10 16:03:15 +0100

    Parse the received JSON according to the selected ID. Idea: get the selected IDs as array, get data from it. Then delete en/de language so we can use the same templates for both languages. Also parse the date according to the language.

---
+++ js/prepareJSON.js
@@ -0,0 +1,42 @@
+/*
+For the given event JSON and the given ID array filter the corresponding entries.
+Accepts language input en/de.
+*/
+
+function(listID, lang)
+{
+
+  // Get raw data of the selected IDs
+  var selectedData;
+  $.getJSON(generateURL('?where={"_id": { \'$in\':' + listID + '\' } }'), function(data) {
+
+  }).done(function(data) {
+    selectedData = data;
+  });
+
+  // Go through JSON and delete all unnecessary language tags. Also add helper
+  // function to get the right date.
+  // Based on https://stackoverflow.com/questions/13391579/how-to-rename-json-key
+  var obj = JSON.parse(selectedData)[0];
+
+  if(lang = 'en')
+  {
+    obj.title = obj.title_en;
+    obj.catchphrase = obj.catchphrase_en;
+    obj.description = obj.description_en;
+
+    delete obj.title_en, obj.catchphrase_en, obj.description_en;
+  }
+  else
+  {
+    obj.title = obj.title_de;
+    obj.catchphrase = obj.catchphrase_de;
+    obj.description = obj.description_de;
+
+    delete obj.title_de, obj.catchphrase_de, obj.description_de;
+  }
+
+  selectedData = JSON.stringify([obj]);
+
+
+}
