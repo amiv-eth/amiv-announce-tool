ffaa4f2b79423679dde45189360bb6502f79b60e
Hermann Blum <hermannsblum@yahoo.de>
2017-12-02 18:06:10 +0100

    update to new api url

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 @brief: Config file
 */
 
-var baseURL = "https://amiv-apidev.vsos.ethz.ch/";
+var baseURL = "https://amiv-api.ethz.ch/";
 var directory = "events";
 
 var mailHandler = "http://192.168.1.9:5000/mailer";
